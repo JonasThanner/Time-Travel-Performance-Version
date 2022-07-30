92c916d8e643769380401c851bfaa5b790108fb9
Argon42043 <argon42043@gmail.com>
2022-07-30 16:58:02 +0200

    added story and home

--- website/time_travel.py
+++ website/time_travel.py
@@ -1,7 +1,32 @@
 from flask import Flask, render_template
+from flask_navigation import Navigation
 
 app = Flask(__name__)
+nav = Navigation(app)
+
+nav.Bar('top', [
+    nav.Item('Home', 'home'),
+    nav.Item('Storry', 'storry'),
+    nav.Item('Instalation', 'instalation'),
+    nav.Item('Tipps', 'tipps')
+])
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/storry")
+def storry():
+    return render_template("storry.html")
+
+
+@app.route("/installation")
+def instalation():
+    return render_template("instalation.html")
+
+
+@app.route("/tipps")
+def tipps():
+    pass
